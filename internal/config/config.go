b212782c0c66e567cd7207d665293afba4b823f2
Egor <egorko25@gmail.com>
2023-02-16 18:14:42 +0300

    изменена структура флагов, добавлены переменные окружения

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,31 +1,39 @@
 package config
 
 import (
+	"encoding/hex"
 	"flag"
+	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
-	Address string
-	Port    string
-	DB      string
+	Address         string `env:"ADDRESS"`
+	DB              string `env:"DB" default:"postgresql://localhost:5432/postgres"`
+	SecretCookieKey []byte `env:"KEY"`
 }
 
 func NewConfig() *Config {
 	var cfg Config
 
+	_ = env.Parse(cfg)
+
+	var secret string
+
 	flag.StringVar(&cfg.Address,
-		"a", "127.0.0.1",
+		"a", "127.0.0.1:8080",
 		"Адрес, на котором располагается сервер",
 	)
 	flag.StringVar(&cfg.DB,
 		"d", "postgresql://localhost:5432/postgres",
 		"Адрес базы данных с которой работает сервер",
 	)
-	flag.StringVar(&cfg.Port,
-		"p", "8080",
-		"Порт на котором работает сервер",
+	flag.StringVar(&secret,
+		"k", " 7de5a1a5ae85e3aef5376333c3410ca984ef56f0c8082f9d6703414c01affect3",
+		"Ключ для шифрования куки",
 	)
 	flag.Parse()
+
+	cfg.SecretCookieKey, _ = hex.DecodeString(secret)
 
 	return &cfg
 }
