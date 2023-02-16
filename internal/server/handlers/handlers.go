1acefbb0b58145574a0f3e30af049c9fa050b9cd
Egor <egorko25@gmail.com>
2023-02-16 20:51:06 +0300

    создал обработчик для авторизации

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -2,28 +2,28 @@ package handlers
 
 import (
 	"bytes"
+	"context"
 	"encoding/gob"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-
 	"gophermart/internal/cookies"
 	"gophermart/internal/database"
 	"gophermart/internal/storage"
+	"io"
+	"log"
+	"net/http"
 )
 
 type Handler struct {
-	db      *database.UserDB
-	coockie *cookies.CookieObj
+	db       *database.UserDB
+	coockies *cookies.CookieObj
 }
 
 func NewHandler(db *database.UserDB, cookies *cookies.CookieObj) *Handler {
 	return &Handler{
-		db:      db,
-		coockie: cookies,
+		db:       db,
+		coockies: cookies,
 	}
 }
 
@@ -42,6 +42,7 @@ func (h *Handler) Balance(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 	var buffer bytes.Buffer
 	var user storage.User
+	var ctx context.Context
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -62,6 +63,12 @@ func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 	}
 
+	err = h.db.InsertUserWithContext(ctx, &user)
+	if err != nil {
+		log.Printf("Ошибка при обращении в бд: \n%e", err)
+		w.WriteHeader(http.StatusConflict)
+	}
+
 	cookie := http.Cookie{
 		Name:  fmt.Sprintf("CookieUser%s", user.Login),
 		Value: user.Login,
@@ -76,11 +83,53 @@ func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Что-то не так:\n%e", errors.New("ошибка упаковки в gob"))
 	}
 
-	h.coockie.WriteEncrypt(w, cookie)
+	h.coockies.WriteEncrypt(w, cookie)
 	w.WriteHeader(http.StatusOK)
 }
 
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
+
+	var user storage.User
+	var ctx context.Context
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Ошибка чтения тела запроса: \n%e", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	defer func() {
+		err = r.Body.Close()
+		if err != nil {
+			log.Printf("Не удалось закрыть тело запроса: \n%e", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}()
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		log.Printf("Ошибка перевода из формата json: \n%e", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	cookiesAll := r.Cookies()
+
+	_, err = h.coockies.ReadEncrypt(r, cookiesAll[0].Name, h.coockies.Key)
+	if err != nil {
+		switch err {
+		case cookies.ErrInvalidValue:
+			log.Printf("Ошибка: %e", errors.New("куки изменены"))
+			w.WriteHeader(http.StatusBadRequest)
+
+		case http.ErrNoCookie:
+			err = h.db.CheckUserWithContext(ctx, &user)
+			if err != nil {
+				log.Printf("Ошибка: %e", err)
+				w.WriteHeader(http.StatusUnauthorized)
+			}
+		}
+
+	}
+
 	w.WriteHeader(http.StatusOK)
 }
 
