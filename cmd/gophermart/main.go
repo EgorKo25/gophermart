5c882601f28606b2d8b32ac07de5ba1a5e6377ac
Egor <egorko25@gmail.com>
2023-02-16 18:09:15 +0300

    Создан прообраз сервера

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"gophermart/internal/cookies"
+	"log"
+	"net/http"
+
 	"gophermart/internal/config"
 	"gophermart/internal/database"
-	"log"
+	"gophermart/internal/server/handlers"
+	"gophermart/internal/server/router"
 )
 
 func main() {
 
-	config := config.NewConfig()
+	cfg := config.NewConfig()
 
-	db := database.NewUserDB(config)
+	db := database.NewUserDB(cfg)
 
-	log.Println(db)
+	coockieFormat := cookies.NewCoockieFormat(cfg.SecretCookieKey)
+
+	handler := handlers.NewHandler(db, coockieFormat)
+
+	myRouter := router.NewRouter(handler)
+
+	log.Println(http.ListenAndServe(cfg.Address, myRouter))
 }
