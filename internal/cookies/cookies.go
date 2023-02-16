0488bc8de0cfaaf81d5ca4c5873fe779f8713b50
Egor <egorko25@gmail.com>
2023-02-16 20:47:14 +0300

    Изменена видиомсть поля Key. Написана функция чтения зашифрованных куки ReadEncrypt

--- internal/cookies/cookies.go
+++ internal/cookies/cookies.go
@@ -11,15 +11,16 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 type CookieObj struct {
-	key []byte
+	Key []byte
 }
 
 func NewCookieObj(key []byte) *CookieObj {
 	return &CookieObj{
-		key: key,
+		Key: key,
 	}
 }
 
@@ -48,9 +49,6 @@ func (c *CookieObj) Read(r *http.Request, name string) (string, error) {
 		return "", err
 	}
 
-	// Decode the base64-encoded cookie value. If the cookie didn't contain a
-	// valid base64-encoded value, this operation will fail and we return an
-	// ErrInvalidValue error.
 	value, err := base64.URLEncoding.DecodeString(cookie.Value)
 	if err != nil {
 		return "", ErrInvalidValue
@@ -61,7 +59,7 @@ func (c *CookieObj) Read(r *http.Request, name string) (string, error) {
 
 func (c *CookieObj) signMyDate(name, value string) string {
 
-	hm := hmac.New(sha256.New, c.key)
+	hm := hmac.New(sha256.New, c.Key)
 	hm.Write([]byte(name))
 	hm.Write([]byte(value))
 	signature := hm.Sum(nil)
@@ -75,7 +73,7 @@ func (c *CookieObj) WriteEncrypt(w http.ResponseWriter, cookie http.Cookie) erro
 
 	cookie.Value = c.signMyDate(cookie.Name, cookie.Value)
 
-	block, err := aes.NewCipher(c.key)
+	block, err := aes.NewCipher(c.Key)
 	if err != nil {
 		return err
 	}
@@ -98,4 +96,46 @@ func (c *CookieObj) WriteEncrypt(w http.ResponseWriter, cookie http.Cookie) erro
 	cookie.Value = string(encryptedValue)
 
 	return c.Write(w, cookie)
+}
+
+func (c *CookieObj) ReadEncrypt(r *http.Request, name string, secretKey []byte) (string, error) {
+	encryptedValue, err := c.Read(r, name)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := aesGCM.NonceSize()
+
+	if len(encryptedValue) < nonceSize {
+		return "", ErrInvalidValue
+	}
+
+	nonce := encryptedValue[:nonceSize]
+	ciphertext := encryptedValue[nonceSize:]
+
+	plaintext, err := aesGCM.Open(nil, []byte(nonce), []byte(ciphertext), nil)
+	if err != nil {
+		return "", ErrInvalidValue
+	}
+
+	expectedName, value, ok := strings.Cut(string(plaintext), ":")
+	if !ok {
+		return "", ErrInvalidValue
+	}
+
+	if expectedName != name {
+		return "", ErrInvalidValue
+	}
+
+	return value, nil
 }
