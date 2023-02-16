480bd61064a824ddb7aa71d0fc4ee2c225c4a3a0
Egor <egorko25@gmail.com>
2023-02-16 18:13:26 +0300

    Изменена структура таблицы users

--- internal/database/database.go
+++ internal/database/database.go
@@ -44,7 +44,7 @@ func createAllTablesWithContext(ctx context.Context, db *sql.DB) error {
 	defer cancel()
 
 	queries := []string{
-		"CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, firstname VARCHAR(20), lastname VARCHAR(20), login VARCHAR(20), passwd VARCHAR(20), token VARCHAR(50), balance BIGINT);",
+		"CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, login VARCHAR(20), passwd VARCHAR(20));",
 		"CREATE TABLE IF NOT EXISTS orders (id SERIAL PRIMARY KEY, order_title VARCHAR(20), user_token VARCHAR(20), balls INT);",
 	}
 
@@ -66,18 +66,15 @@ func (db *UserDB) InsertNewUserWithContext(ctx context.Context, user *storage.Us
 		return errors.New("отсутствует открытая база данных")
 	}
 
-	query := `INSERT INTO users (id, firstname, lastname, login, passwd)
-							VALUES(@id, @firstname, @lastname, @login, @passwd);`
+	query := `INSERT INTO users (login, passwd)
+							VALUES(@login, @passwd);`
 
 	r, err := db.db.ExecContext(childCtx, query,
-		sql.Named("id", user.Firstname),
-		sql.Named("firstname", user.Firstname),
-		sql.Named("lastname", user.Lastname),
 		sql.Named("login", user.Login),
 		sql.Named("passwd", user.Passwd),
 	)
 	if err != nil {
-		errors.New(fmt.Sprintf("не удалось отправить данные в базу данных.\n Ошибка: %s\nОтвет базы данных: %s", err, r))
+		_ = errors.New(fmt.Sprintf("не удалось отправить данные в базу данных.\n Ошибка: %s\nОтвет базы данных: %s", err, r))
 	}
 
 	return nil
