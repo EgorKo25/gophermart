81d813f61ec78db32d8bd13a6636ee1955550242
Egor <egorko25@gmail.com>
2023-02-17 00:50:51 +0300

    добавлена отладочная информация

--- internal/database/database.go
+++ internal/database/database.go
@@ -28,7 +28,7 @@ func NewUserDB(cfg *config.Config) *UserDB {
 		log.Println("Не возожно подключиться к бд: ", err)
 	}
 
-	log.Printf("\n%s\n%s\n%s\n%s", db, cfg.DB, cfg.Address, cfg.BlackBox)
+	log.Printf("\n\n%s\n%s\n%s", cfg.DB, cfg.Address, cfg.BlackBox)
 	err = createAllTablesWithContext(ctx, db)
 	if err != nil {
 		log.Println(err)
