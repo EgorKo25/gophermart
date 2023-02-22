74d41048a7b9a72e3397c9bd80ede51c7651c0b1
Egor <egorko25@gmail.com>
2023-02-22 04:11:13 +0300

    добавлена отладочная информация

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -216,6 +216,9 @@ func (h *Handler) Orders(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	case nil:
 		err = h.db.InsertOrderWithContext(ctx, &order)
+		if err != nil {
+			log.Println(err)
+		}
 		w.WriteHeader(http.StatusAccepted)
 
 		err = h.checkOrderStatus(&order)
